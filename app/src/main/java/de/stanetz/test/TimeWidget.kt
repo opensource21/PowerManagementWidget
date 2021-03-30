f1d9691075edc05eb7714ef982c09038997902b4
niels <opensource21@gmail.com>
2021-03-30 21:29:14 +0200

    First Hack to get the time and show it.

--- app/src/main/java/de/stanetz/test/TimeWidget.kt
+++ app/src/main/java/de/stanetz/test/TimeWidget.kt
@@ -3,7 +3,13 @@ package de.stanetz.test
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
+import android.os.StrictMode
+import android.util.Log
 import android.widget.RemoteViews
+import java.net.HttpURLConnection
+import java.net.URL
+import java.util.*
+
 
 /**
  * Implementation of App Widget functionality.
@@ -14,9 +20,16 @@ class TimeWidget : AppWidgetProvider() {
         appWidgetManager: AppWidgetManager,
         appWidgetIds: IntArray
     ) {
+        // TODO niels 30.03.21: Better use https://developer.android.com/topic/libraries/architecture/coroutines
+        val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+
+        StrictMode.setThreadPolicy(policy)
+        val url = URL("http://www.google.de/")
+        val urlConnection: HttpURLConnection = url.openConnection() as HttpURLConnection
+        val currentDate = Date(urlConnection.date)
         // There may be multiple widgets active, so update all of them
         for (appWidgetId in appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId)
+            updateAppWidget(context, appWidgetManager, appWidgetId, currentDate)
         }
     }
 
@@ -32,12 +45,14 @@ class TimeWidget : AppWidgetProvider() {
 internal fun updateAppWidget(
     context: Context,
     appWidgetManager: AppWidgetManager,
-    appWidgetId: Int
+    appWidgetId: Int,
+    currentDate: Date
 ) {
     val widgetText = context.getString(R.string.appwidget_text)
+    Log.i("PowerTestWidget", "updateAppWidget: $currentDate")
     // Construct the RemoteViews object
     val views = RemoteViews(context.packageName, R.layout.time_widget)
-    views.setTextViewText(R.id.appwidget_text, widgetText)
+    views.setTextViewText(R.id.appwidget_text, "Now: $currentDate")
 
     // Instruct the widget manager to update the widget
     appWidgetManager.updateAppWidget(appWidgetId, views)
